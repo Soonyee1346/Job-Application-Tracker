86b671207669ee668649669a744c3e7a8e2de1a2
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2026-02-19 14:32:13 +1100

    APIs

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:jobtracker@localhost:5432/job_tracker"
+DATABASE_URL = "postgresql://postgres:jobtracker@localhost:5444/job_tracker"
 
 engine = create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
