c3b67a415a30d9211c5d7313777977049412bc24
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2026-03-01 14:34:10 +1100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,6 +39,12 @@ resource "aws_ecs_task_definition" "backend" {
                     hostPort      = 8000
                 }
             ]
+            environment  = [
+                {
+                    name = "DATABASE_URL"
+                    value = "postgresql://postgres:jobtracker@{aws_db_instance.postgres.endopoint}/job_tracker
+                }
+            ]
         }
     ])
 }
@@ -57,10 +63,38 @@ resource "aws_ecs_service" "backend" {
     }
 }
 
+resource "aws_security_group" "db_sg" {
+    name            = "${var.project_name}-db-sg"
+    description     = "Allow inbound traffic from backend only"
+    vpc_id          = aws_vpc.main.id
+
+    ingress {
+        from_port           = 5432
+        to_port             = 5432
+        protocol            = "tcp"
+        security_groups     = [aws_security_group.backend_sg.id]
+    }
+
+    egress {
+        from_port           = 0
+        to_port             = 0
+        protocol            = "-1"
+        cidr_blocks         = ["0.0.0.0/0"]
+
+    }
+}
+
 resource "aws_db_instance" "postgres" {
-    identifier           = "${var.project_name}-db"
-    instance_class       = "db.t3.micro"
-    engine               = "postgres"
-    db_subnet_group_name = aws_db_subnet_group.main.name
+    identifier             = "${var.project_name}-db"
+    allocated_storage      = 20
+    storage_type           = "gp2"
+    instance_class         = "db.t3.micro"
+    engine                 = "postgres"
+    engine_version         = "15.4"
+    db_name                = "job_tracker"
+    username               = "postgres"
+    password               = "jobtracker" # Use a secrets manager in prod
+    db_subnet_group_name   = aws_db_subnet_group.main.name
     vpc_security_group_ids = [aws_security_group.db_sg.id]
+    skip_final_snapshot    = true
 }
