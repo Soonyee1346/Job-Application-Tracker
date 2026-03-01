5654beb7e2c11faaf16fd844949a0760c6dcb381
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2026-03-01 14:14:23 +1100

    Docker + terraform

---
+++ terraform/main.tf
@@ -0,0 +1,66 @@
+resource "aws_ecs_cluster" "main" {
+    name = "${var.project_name}-cluster"
+}
+
+resource "aws_security_group" "backend_sg" {
+    name        = "${var.project_name}-backend-sg"
+    vpc_id      = aws_vpc.main.id
+
+    ingress {
+        from_port   = 8000
+        to_port     = 8000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] # Restrict to loadbalancer
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_ecs_task_definition" "backend" {
+    family                   = "backend"
+    network_mode             = "awsvpc"
+    requires_compatabilities = ["FARGATE"]
+    cpu                      = "256"
+    memory                   = "512"
+
+    container_definitions = jsonencode([
+        {
+            name         = "backend"
+            image        = "YOUR_AWS_ACCOUNT_ID.dkr.ecr.ap-southeast-2.amazonaws.com/job-tracker-backend:latest"
+            essential    = true
+            portMappings = [
+                {
+                    containerPort = 8000
+                    hostPort      = 8000
+                }
+            ]
+        }
+    ])
+}
+
+resource "aws_ecs_service" "backend" {
+    name            = "backend-service"
+    cluster         = aws_ecs_cluster.main.id
+    task_definition = aws_ecs_task_definition.backend.arn
+    desired_count   = 1
+    launch_type     = "FARGATE"
+
+    network_configuration {
+        subnets          = aws_subnet.public[*].id
+        security_groups   = [aws_security_group.backend_sg.id]
+        assign_public_ip = true
+    }
+}
+
+resource "aws_db_instance" "postgres" {
+    identifier           = "${var.project_name}-db"
+    instance_class       = "db.t3.micro"
+    engine               = "postgres"
+    db_subnet_group_name = aws_db_subnet_group.main.name
+    vpc_security_group_ids = [aws_security_group.db_sg.id]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+    description = "AWS Region"
+    default     = "ap-southeast-2"
+}
+
+variable "project_name" {
+    description = "Name of the project"
+    default     = "job-tracker"
+}
